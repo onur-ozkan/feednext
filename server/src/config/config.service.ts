dad2bc27d713a663733d9c0275308364aadf02ba
Onur Özkan <onurozkan.dev@outlook.com>
2019-10-09 23:32:54 +0300

    Initialize config module and service

---
+++ server/src/config/config.service.ts
@@ -0,0 +1,15 @@
+
+import * as dotenv from 'dotenv';
+import * as fs from 'fs';
+
+export class ConfigService {
+  private readonly envConfig: { [key: string]: string };
+
+  constructor(filePath: string) {
+    this.envConfig = dotenv.parse(fs.readFileSync(filePath));
+  }
+
+  get(key: string): string {
+    return this.envConfig[key];
+  }
+}
