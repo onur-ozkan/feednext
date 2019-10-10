9891c3eb318ff28712feda5b4345bbe7477ad776
Onur Özkan <onurozkan.dev@outlook.com>
2019-10-10 12:03:18 +0300

    Create entity of Products

---
+++ server/src/products/products.entity.ts
@@ -0,0 +1,25 @@
+import { Column, Entity, ObjectID, ObjectIdColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity()
+export class Product {
+  @ObjectIdColumn()
+  id: ObjectID;
+
+  @Column()
+  categoryId: ObjectID;
+
+  @Column({
+    type: 'varchar',
+    length: 150,
+  })
+  name: string;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp'})
+  updatedAt?: Date;
+
+  @Column({ type: 'timestamp' })
+  deletedAt?: Date;
+}
