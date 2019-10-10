52ea737d3fbc068b4287bd4b2f5853845b5f9988
Onur Özkan <onurozkan.dev@outlook.com>
2019-10-10 12:07:26 +0300

    Create entity of Users

---
+++ server/src/users/users.entity.ts
@@ -0,0 +1,31 @@
+import { Column, Entity, ObjectID, ObjectIdColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @ObjectIdColumn()
+  id: ObjectID;
+
+  @Column()
+  categoryId: ObjectID;
+
+  @Column({
+    type: 'varchar',
+    length: 17,
+  })
+  username: string;
+
+  @Column({
+    type: 'varchar',
+    length: 50,
+  })
+  fullName: string;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp'})
+  updatedAt?: Date;
+
+  @Column({ type: 'timestamp' })
+  deletedAt?: Date;
+}
