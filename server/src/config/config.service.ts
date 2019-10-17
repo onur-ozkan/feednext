4117978ce0a2c36570a72ee4e4de90b8b3b583d6
ozkan.onur <onurozkan.dev@outlook.com>
2019-10-18 00:30:26 +0300

    Make database & environment variables connection

--- server/src/config/config.service.ts
+++ server/src/config/config.service.ts
@@ -1,15 +1,60 @@
 
-import * as dotenv from 'dotenv';
-import * as fs from 'fs';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { User } from '../users/users.entity';
+
+// tslint:disable-next-line:no-var-requires
+require('dotenv').config();
 
 export class ConfigService {
-  private readonly envConfig: { [key: string]: string };
+  constructor(private env: { [k: string]: string | undefined }) { }
 
-  constructor(filePath: string) {
-    this.envConfig = dotenv.parse(fs.readFileSync(filePath));
+  private get(key: string, throwOnMissing = true): string {
+    const value = this.env[key];
+    if (!value && throwOnMissing) {
+      throw new Error(`config error - missing env.${key}`);
+    }
+
+    return value;
   }
 
-  get(key: string): string {
-    return this.envConfig[key];
+  public ensureValues(keys: string[]) {
+    keys.forEach(k => this.get(k, true));
+    return this;
+  }
+
+  public getPort() {
+    return this.get('APP_PORT', true);
+  }
+
+  public isProduction() {
+    const mode = this.get('MODE', false);
+    return mode !== 'DEV';
+  }
+
+  public getTypeOrmConfig(): TypeOrmModuleOptions {
+    return {
+      type: 'mongodb',
+
+      host: this.get('DB_HOST'),
+      database: this.get('DB_NAME'),
+      synchronize: true,
+
+      entities: [User],
+      // migrationsTableName: 'migration',
+      // migrations: ['../migration/*.ts'],
+      // cli: {migrationsDir: '../migration'},
+
+      ssl: this.isProduction(),
+    };
   }
 }
+
+const databaseService = new ConfigService(process.env)
+  .ensureValues([
+    'DB_HOST',
+    'DB_NAME',
+  ]);
+
+const configService = new ConfigService(process.env);
+
+export { databaseService, configService };
