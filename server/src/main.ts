077523250f55147fe1f6b3c744e196f7ec1d082f
ozkan.onur <onurozkan.dev@outlook.com>
2019-10-18 00:31:40 +0300

    Listen app port from configService

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,6 +4,7 @@ import * as cookieParser from 'cookie-parser';
 import * as helmet from 'helmet';
 import * as csurf from 'csurf';
 import * as rateLimit from 'express-rate-limit';
+import { configService } from './config/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -24,6 +25,6 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(app.get('ConfigService').get('APP_PORT'));
+  await app.listen(configService.getPort());
 }
 bootstrap();
