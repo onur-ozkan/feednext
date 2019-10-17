2181d71d08b6ff97ea429e43b0183f68711ca567
ozkan.onur <onurozkan.dev@outlook.com>
2019-10-18 00:33:15 +0300

    Remove unused imports

--- server/src/entries/entries.module.ts
+++ server/src/entries/entries.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
-import { DatabaseModule } from '../database/database.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [],
 })
 export class EntriesModule {}
