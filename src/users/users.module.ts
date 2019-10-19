fea5ba214f807c5f652cde29f50aa51b76d76779
Onur Özkan <onurozkan.dev@outlook.com>
2019-10-19 13:29:51 +0300

    Optimization

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,11 +3,11 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { UserEntity } from './users.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService } from '../config/config.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity])],
-  providers: [UsersService],
-  exports: [UsersService],
+  providers: [UsersService, ConfigService],
   controllers: [UsersController],
 })
 export class UsersModule {}
