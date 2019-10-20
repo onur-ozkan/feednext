1f6c48d122ec5ea797403b1866d9844b2fb6a9ac
ozkanonur <onurozkan.dev@outlook.com>
2019-10-20 18:14:08 +0300

    Import auth model to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 
 import { databaseService } from './shared/config/config.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     UsersModule,
     TypeOrmModule.forRoot(databaseService.getTypeOrmConfig()),
+    AuthModule,
   ],
 })
 
