81ff95f67bb7f1ef1855a7a6ec061e1a1a5e35eb
ozkanonur <onurozkan.dev@outlook.com>
2019-10-20 19:08:43 +0300

    Create jwt auth strategy

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,19 @@
+import {ExtractJwt, Strategy} from 'passport-jwt';
+import {PassportStrategy} from '@nestjs/passport';
+import {Injectable} from '@nestjs/common';
+import { configService } from '../shared/config/config.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor() {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey: configService.getSecretKey(),
+        });
+    }
+
+    async validate(payload: any) {
+        return {userId: payload.sub, email: payload.email};
+    }
+}
