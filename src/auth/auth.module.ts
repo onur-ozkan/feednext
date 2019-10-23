18c0b21f76e83ba551603fd8cd98dcd653faf296
ozkanonur <onurozkan.dev@outlook.com>
2019-10-23 23:07:42 +0300

    Re-configure auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,31 +2,38 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './Service/auth.service';
 import { UsersModule } from '../users/users.module';
 import { PassportModule } from '@nestjs/passport';
-import { LocalStrategy } from './Strategy/local.strategy';
 import { AuthController } from './Controller/auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { configService } from '../shared/Config/app.config';
 import { JwtStrategy } from './Strategy/jwt.strategy';
 import { UserEntity } from '../users/Entity/users.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersService } from '../users/Service/users.service';
 
 @Module({
     imports: [
         UsersModule,
-        PassportModule,
-        JwtModule.register({
-            secret: configService.getSecretKey(),
-            signOptions: { expiresIn: '3600s' },
-        }),
+        PassportModule.register({ defaultStrategy: 'jwt' }),
+        JwtModule.registerAsync({
+            useFactory: async () => {
+              return {
+                secret: configService.getSecretKey(),
+                signOptions: {
+                  ...(
+                    configService.getJwtExpireTime() ? { expiresIn: Number(configService.getJwtExpireTime()) } : {}
+                  ),
+                },
+              };
+            },
+          }),
         TypeOrmModule.forFeature([UserEntity]),
     ],
     providers: [
         AuthService,
-        LocalStrategy,
         JwtStrategy,
-        UsersService,
     ],
     controllers: [AuthController],
+    exports: [
+        PassportModule.register({ defaultStrategy: 'jwt' }),
+    ],
 })
 export class AuthModule {}
