9f0ca7950507a2ef1d0f5c0b36eb6adb19953520
ozkanonur <onurozkan.dev@outlook.com>
2019-10-23 23:11:48 +0300

    Control the endpoints ['signin', 'signup', 'me']

--- src/auth/Controller/auth.controller.ts
+++ src/auth/Controller/auth.controller.ts
@@ -1,22 +1,39 @@
-import { Controller, Post, UseGuards, Request, Body, UsePipes } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { Controller, Post, Body, UsePipes, UseGuards, Get, Request } from '@nestjs/common';
+import { ApiResponse, ApiBearerAuth } from '@nestjs/swagger';
 import { AuthService } from '../Service/auth.service';
 import { CreateUserDto } from '../../users/Dto/create-user.dto';
 import { ValidationPipe } from '../../shared/Pipe/validation.pipe';
+import { LoginDto } from '../Dto/login.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
-    @UseGuards(AuthGuard('local'))
-    @Post('login')
-    async login(@Request() req) {
-        return this.authService.login(req.user);
+    @Post('signin')
+    @ApiResponse({ status: 201, description: 'Successfully Signed In' })
+    @ApiResponse({ status: 400, description: 'Bad Request' })
+    @ApiResponse({ status: 401, description: 'Unauthorized' })
+    async login(@Body() dto: LoginDto): Promise<any> {
+        const user = await this.authService.validateUser(dto);
+        return await this.authService.createToken(user);
     }
 
-    @Post('register')
+    @Post('signup')
+    @ApiResponse({ status: 201, description: 'Successfully Signed Up' })
+    @ApiResponse({ status: 400, description: 'Bad Request' })
+    @ApiResponse({ status: 401, description: 'Unauthorized' })
     @UsePipes(new ValidationPipe())
-    async register(@Body() dto: CreateUserDto) {
+    async register(@Body() dto: CreateUserDto): Promise<any> {
         return this.authService.register(dto);
+    }
+
+    @ApiBearerAuth()
+    @UseGuards(AuthGuard())
+    @Get('me')
+    @ApiResponse({ status: 200, description: 'Successful Response' })
+    @ApiResponse({ status: 401, description: 'Unauthorized' })
+    async getLoggedInUser(@Request() request): Promise<any> {
+        return request.user;
     }
 }
