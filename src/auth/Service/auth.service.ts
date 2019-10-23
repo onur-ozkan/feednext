c1122502ce63415092cd18891e7132fd1263c2d6
ozkanonur <onurozkan.dev@outlook.com>
2019-10-23 23:12:39 +0300

    Validate the user and create token

--- src/auth/Service/auth.service.ts
+++ src/auth/Service/auth.service.ts
@@ -1,26 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../../users/Service/users.service';
 import { JwtService } from '@nestjs/jwt';
+import { configService } from '../../shared/Config/app.config';
 import { CreateUserDto } from '../../users/Dto/create-user.dto';
+import { UserEntity } from '../../users/Entity/users.entity';
+import { LoginDto } from '../Dto/login.dto';
 
 @Injectable()
 export class AuthService {
     constructor(private readonly userService: UsersService,
                 private readonly jwtService: JwtService) {}
 
-    async validateUser(userName: string, pass: string) {
-        const user: any = await this.userService.findOne(userName);
-        if (user && user.password === pass) {
-            const {password, ...result} = user;
-            return result;
+    async validateUser(dto: LoginDto): Promise<any> {
+        const user = await this.userService.userValidation(dto.email, dto.username, dto.password);
+        if (!user) {
+            throw new UnauthorizedException('User does not exist.');
         }
-        return null;
+        return user;
     }
 
-    async login(user: any) {
-        const payload = {email: user.email, sub: user.userId};
+    async createToken(userEntity: UserEntity): Promise<any> {
         return {
-            accessToken: this.jwtService.sign(payload),
+          expiresIn: configService.getJwtExpireTime(),
+          accessToken: this.jwtService.sign({_id: userEntity._id}),
+          user: userEntity,
         };
     }
 
