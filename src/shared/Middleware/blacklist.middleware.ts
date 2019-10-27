10e0079148013d418fbc32ca6ea64a0f8a963f97
ozkanonur <onurozkan.dev@outlook.com>
2019-10-27 11:49:33 +0300

    Remove comment line

--- src/shared/Middleware/blacklist.middleware.ts
+++ src/shared/Middleware/blacklist.middleware.ts
@@ -5,7 +5,7 @@ import { RedisService } from '../Redis/redis.service';
 @Injectable()
 export class BlacklistMiddleware implements NestMiddleware {
   constructor(private readonly redisService: RedisService) {}
-  // tslint:disable-next-line:ban-types
+
   async use(req: Request, res: Response, next: Function) {
     const token = req.headers.authorization.substring(7);
     const isTokenDead = await this.redisService.getData(token);
