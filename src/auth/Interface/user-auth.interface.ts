54c26c62caf0a828a69eab51642be50350ce6f15
ozkanonur <onurozkan.dev@outlook.com>
2019-10-28 11:43:20 +0300

    Create user-auth interface

---
+++ src/auth/Interface/user-auth.interface.ts
@@ -0,0 +1,10 @@
+export interface UserAuthData {
+  fullName: string;
+  username: string;
+  password: string;
+  email: string;
+}
+
+export interface UserAuthInterface {
+  user: UserAuthData;
+}
