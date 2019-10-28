cd420720c4fa0b3eda79f9c089b9495637d296c6
ozkanonur <onurozkan.dev@outlook.com>
2019-10-28 11:43:41 +0300

    Improve auth process

--- src/auth/Service/auth.service.ts
+++ src/auth/Service/auth.service.ts
@@ -1,20 +1,21 @@
-import { Injectable, UnauthorizedException, BadRequestException } from '@nestjs/common';
-import { UsersService } from '../../users/Service/users.service';
+import { Injectable, BadRequestException, UnprocessableEntityException, NotFoundException, HttpStatus, HttpException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { configService } from '../../shared/Config/config.service';
-import { CreateUserDto } from '../../users/Dto/create-user.dto';
+import { CreateUserDto } from '../Dto/create-user.dto';
 import { UserEntity } from '../../users/Entity/users.entity';
 import { LoginDto } from '../Dto/login.dto';
 import { RedisService } from '../../shared/Redis/redis.service';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Validator } from 'class-validator';
+import { VerifyEmailDto } from '../Dto/verify-email.dto';
+import { Serializer } from 'jsonapi-serializer';
+import { UserAuthInterface } from '../Interface/user-auth.interface';
 import * as crypto from 'crypto';
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly userService: UsersService,
-                private readonly jwtService: JwtService,
+    constructor(private readonly jwtService: JwtService,
                 private readonly redisService: RedisService,
                 @InjectRepository(UserEntity)
                 private readonly userRepository: Repository<UserEntity>,
@@ -24,26 +25,37 @@ export class AuthService {
         const passHash = crypto.createHmac('sha256', dto.password).digest('hex');
 
         if (dto.email) {
-            if (!await this.validateEmail(dto.email)) {
-                throw new BadRequestException('Email is not an email.');
-            }
+            await this.validateEmail(dto.email);
             try {
                 return await this.userRepository.findOneOrFail({email: dto.email, password: passHash});
             } catch (err) {
-                throw new UnauthorizedException('User does not exist.');
+                throw new NotFoundException('User does not exist.');
             }
         }
 
         try {
             return await this.userRepository.findOneOrFail({username: dto.username, password: passHash});
         } catch (err) {
-            throw new UnauthorizedException('User does not exist.');
+            throw new NotFoundException('User does not exist.');
         }
     }
 
-    async validateEmail(incEmail: string): Promise<boolean> {
+    async verifyEmail(dto: VerifyEmailDto): Promise<HttpException> {
+        await this.validateEmail(dto.email);
+        try {
+            await this.userRepository.findOneOrFail({email: dto.email});
+        } catch (err) {
+            throw new NotFoundException('Email does not exist in the database.');
+        }
+        throw new HttpException('OK', HttpStatus.OK);
+    }
+
+    async validateEmail(incEmail: string): Promise<any> {
         const validator = new Validator();
-        return validator.isEmail(incEmail);
+        if (validator.isEmail(incEmail)) {
+            return;
+        }
+        throw new BadRequestException('Email is not an email.');
     }
 
     async createToken(userEntity: UserEntity): Promise<any> {
@@ -71,7 +83,20 @@ export class AuthService {
         await this.redisService.setOnlyKey(token, remainingSeconds);
     }
 
-    async register(dto: CreateUserDto): Promise<any> {
-        return await this.userService.create(dto);
-    }
+    async register(dto: CreateUserDto): Promise<UserAuthInterface> {
+        // Create new user
+        const newUser = new UserEntity({
+          email: dto.email,
+          username: dto.username,
+          password: dto.password,
+          fullName: dto.fullName,
+        });
+
+        try {
+          const result = await this.userRepository.save(newUser);
+          return await new Serializer('user-identities', { attributes: ['fullName', 'username', 'email', 'accessToken'] }).serialize(result);
+        } catch (err) {
+          throw new UnprocessableEntityException(err.errmsg);
+        }
+      }
 }
