54b7047b128dd4af4d319b0f59ab394d4972c561
ozkanonur <onurozkan.dev@outlook.com>
2019-10-28 11:43:52 +0300

    Optimize process

--- src/users/Service/users.service.ts
+++ src/users/Service/users.service.ts
@@ -2,9 +2,7 @@ import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { UserEntity } from '../Entity/users.entity';
-import { UserInterface } from '../Interface/user.interface';
 import { Serializer } from 'jsonapi-serializer';
-import { CreateUserDto } from '../Dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -26,23 +24,4 @@ export class UsersService {
       throw new HttpException(err, HttpStatus.NOT_FOUND);
     }
   }
-
-  // User registration process logic
-  async create(dto: CreateUserDto): Promise<UserInterface> {
-    // Create new user
-    const newUser = new UserEntity({
-      email: dto.email,
-      username: dto.username,
-      password: dto.password,
-      fullName: dto.fullName,
-    });
-
-    try {
-      const result = await this.userRepository.save(newUser);
-      return await new Serializer('user-identities', { attributes: ['fullName', 'username', 'email', 'accessToken'] }).serialize(result);
-    } catch (err) {
-      throw new HttpException(err, HttpStatus.UNPROCESSABLE_ENTITY);
-    }
-  }
-
 }
