bb012151c04ffee1905c3c2b0ca38ecd65a9c477
Onur Özkan <onurozkan.dev@outlook.com>
2019-11-03 14:07:04 +0300

    Do typecheck

--- src/auth/Service/auth.service.ts
+++ src/auth/Service/auth.service.ts
@@ -1,15 +1,13 @@
 import {
     Injectable,
-    BadRequestException,
     UnprocessableEntityException,
     NotFoundException,
     HttpStatus,
     HttpException,
 } from '@nestjs/common'
-import { JwtService } from '@nestjs/jwt'
+import { JwtService, JwtModule } from '@nestjs/jwt'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Serializer } from 'jsonapi-serializer'
-import { Validator } from 'class-validator'
 import { Repository } from 'typeorm'
 import { configService } from '../../shared/Config/config.service'
 import { CreateUserDto } from '../Dto/create-user.dto'
@@ -33,13 +31,13 @@ export class AuthService {
     ) {}
 
     async validateUser(dto: LoginDto): Promise<any> {
-        const decodedPass = crypto.createHmac(`sha256`, dto.password).digest(`hex`)
+        const passwordHash: string = crypto.createHmac(`sha256`, dto.password).digest(`hex`)
 
         if (dto.email) {
             try {
-                await this.userRepository.findOneOrFail({
+                return await this.userRepository.findOneOrFail({
                     email: dto.email,
-                    password: decodedPass,
+                    password: passwordHash,
                 })
             } catch (err) {
                 throw new NotFoundException(`Couldn't find an account that matching with this email and password in the database.`)
@@ -47,9 +45,8 @@ export class AuthService {
         }
 
         try {
-            await this.userRepository.findOneOrFail({
-                username: dto.username,
-                password: decodedPass,
+            return await this.userRepository.findOneOrFail({
+                password: passwordHash,
             })
         } catch (err) {
             throw new NotFoundException(`Couldn't find an account that matching with this email and password in the database.`)
@@ -58,20 +55,20 @@ export class AuthService {
 
     async accountRecovery(dto: AccountRecoveryDto): Promise<HttpException> {
         try {
-            const account = await this.userRepository.findOneOrFail({ email: dto.email })
-            const generatePassword = await kmachine.keymachine()
+            const account: UserEntity = await this.userRepository.findOneOrFail({ email: dto.email })
+            const generatePassword: string = await kmachine.keymachine()
             account.password = generatePassword
             await this.userRepository.save(account)
 
-            const mailBody = {
-                receiver: dto.email,
+            const mailBody: EmailSenderBody = {
+                receiver: 'rjergbohn@gmail.com',
                 subject: `Account Recovery [${account.username}]`,
                 text: `By your request we have set your password as '${generatePassword}' for x hours, in that time please sign in and update your Account Password.`,
             }
 
             await this.sendMail(mailBody)
         } catch (err) {
-            throw new NotFoundException(`This email is not exist in the database.`)
+            throw new NotFoundException(`This email does not exist in the database.`)
         }
         throw new HttpException(`OK`, HttpStatus.OK)
     }
@@ -79,14 +76,14 @@ export class AuthService {
     async accountVerification(incToken: string): Promise<HttpException> {
         const decodedToken: any = this.jwtService.decode(incToken)
         if (decodedToken) {
-            const remainingTime = await decodedToken.exp - Math.floor(Date.now() / 1000)
+            const remainingTime: number = await decodedToken.exp - Math.floor(Date.now() / 1000)
             if (remainingTime <= 0) {
                 throw new NotFoundException(`Incoming token is expired.`)
             }
         }
 
         try {
-            const account = await this.userRepository.findOneOrFail({ email: decodedToken.email })
+            const account: UserEntity = await this.userRepository.findOneOrFail({ email: decodedToken.email })
             account.isVerified = true
             await this.userRepository.save(account)
         } catch (err) {
@@ -95,49 +92,40 @@ export class AuthService {
         throw new HttpException(`Account has been verified.`, HttpStatus.OK)
     }
 
-    async validateEmail(incEmail: string): Promise<any> {
-        const validator = new Validator()
-        if (validator.isEmail(incEmail)) {
-            return
-        }
-        throw new BadRequestException(`Email is not an email.`)
-    }
-
     async createToken(userEntity: UserEntity): Promise<HttpException> {
-        const token = this.jwtService.sign({
+        const token: string = this.jwtService.sign({
             _id: userEntity._id,
             username: userEntity.username,
             email: userEntity.email,
             createdAt: userEntity.createdAt,
         })
 
-        const responseData = {
+        const responseData: object = {
             expiresIn: configService.get(`JWT_EXPIRATION_TIME`),
             accessToken: token,
             user: userEntity,
         }
-        throw new HttpException({statusCode: 200, message: `Auth token has been generated.`, data: responseData}, HttpStatus.OK )
+        throw new HttpException({statusCode: 200, message: `Access token has been generated.`, data: responseData}, HttpStatus.OK )
     }
 
     async killToken(token: string): Promise<HttpException> {
         const decodedToken: any = this.jwtService.decode(token)
-        const expireDate = decodedToken.exp
-        const remainingSeconds = Math.round(expireDate - Date.now() / 1000)
+        const expireDate: number = decodedToken.exp
+        const remainingSeconds: number = Math.round(expireDate - Date.now() / 1000)
 
         await this.redisService.setOnlyKey(token, remainingSeconds)
         throw new HttpException(`Token has been destroyed.`, HttpStatus.OK)
     }
 
     async register(dto: CreateUserDto): Promise<HttpException> {
-        // Create new user
-        const newUser = new UserEntity({
+        const newUser: UserEntity = new UserEntity({
             email: dto.email,
             username: dto.username,
             password: dto.password,
             fullName: dto.fullName,
         })
 
-        let result: any
+        let result: object
 
         try {
             result = await this.userRepository.save(newUser)
@@ -148,27 +136,27 @@ export class AuthService {
             throw new UnprocessableEntityException(err.errmsg)
         }
 
-        const verifyToken = jwt.sign({
+        const verifyToken: JwtModule = jwt.sign({
             id: dto.username,
             email: dto.email,
             exp: Math.floor(Date.now() / 1000) + (15 * 60), // Token expires in 15 min
         }, configService.get(`SECRET_KEY`))
 
-        const verificationUrl = `${configService.get(`APP_URL`)}/api/v1/auth/account-verification?token=${verifyToken}`
+        const verificationUrl: string = `${configService.get(`APP_URL`)}/api/v1/auth/account-verification?token=${verifyToken}`
 
-        const mailBody = {
+        const mailBody: EmailSenderBody = {
             receiver: dto.email,
             subject: `Verify Your Account [${dto.username}]`,
             text: `${verificationUrl}`,
         }
         await this.sendMail(mailBody)
 
-        const data = result.data
+        const data: object = result['data']
         throw new HttpException({statusCode: 200, message: `Account has been registered successfully to the database.`, data}, HttpStatus.OK)
     }
 
     private async sendMail(bodyData: EmailSenderBody) {
-        const transporter = await nodemailer.createTransport({
+        const transporter: nodemailer = await nodemailer.createTransport({
             service: configService.get(`NODEMAILER_SERVICE`),
             auth: {
                 user: configService.get(`NODEMAILER_MAIL`),
@@ -176,7 +164,7 @@ export class AuthService {
             },
         })
 
-        const mailOptions = {
+        const mailOptions: object = {
             from: configService.get(`NODEMAILER_MAIL`),
             to: bodyData.receiver,
             subject: bodyData.subject,
