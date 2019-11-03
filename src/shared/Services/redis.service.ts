845ce4662873e0e18e7bab6b105c31ebf0c548e9
Onur Özkan <onurozkan.dev@outlook.com>
2019-11-03 21:58:59 +0300

    Move shared services to the Services folder

---
+++ src/shared/Services/redis.service.ts
@@ -0,0 +1,41 @@
+import { configService } from './config.service'
+import * as Redis from 'ioredis'
+
+export class RedisService {
+    private redisConnection() {
+        return new Redis({
+            host: configService.get('REDIS_HOST'),
+            port: configService.get('REDIS_PORT'),
+            password: configService.get('REDIS_PASSWORD'),
+        })
+    }
+
+    async setData(key: any, value: any, expireTime?: number) {
+        if (expireTime) {
+            await this.redisConnection().set(key, value, 'EX', expireTime)
+        } else {
+            await this.redisConnection().set(key, value)
+        }
+    }
+
+    async setOnlyKey(key: any, expireTime?: number) {
+        if (expireTime) {
+            await this.redisConnection().set(key, null, 'EX', expireTime)
+        } else {
+            await this.redisConnection().set(key, null)
+        }
+    }
+
+    async pushList(key: any, value: any, expireTime?: number) {
+        if (expireTime) {
+            await this.redisConnection().rpush(key, value)
+            await this.redisConnection().expire(key, expireTime)
+        } else {
+            await this.redisConnection().rpush(key, value)
+        }
+    }
+
+    async getData(key: any) {
+        return await this.redisConnection().get(key)
+    }
+}
