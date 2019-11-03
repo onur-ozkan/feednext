3afe851e338720021b848de8642799a174fa90e8
Onur Özkan <onurozkan.dev@outlook.com>
2019-11-03 21:43:21 +0300

    Make own folder for entities

---
+++ src/shared/Entities/users.entity.ts
@@ -0,0 +1,69 @@
+import {
+    Column,
+    Entity,
+    ObjectID,
+    ObjectIdColumn,
+    CreateDateColumn,
+    UpdateDateColumn,
+    BeforeInsert,
+    BeforeUpdate,
+} from 'typeorm'
+import { IsEmail } from 'class-validator'
+import * as crypto from 'crypto'
+
+@Entity({ name: 'Users' })
+export class UserEntity {
+    constructor(partial: Partial<UserEntity>) {
+        Object.assign(this, partial)
+    }
+
+    @ObjectIdColumn()
+    _id: ObjectID
+
+    @Column({
+        length: 50,
+    })
+    fullName: string
+
+    @Column({
+        length: 17,
+        unique: true,
+    })
+    username: string
+
+    @Column({
+        length: 15,
+    })
+    password: string
+
+    @BeforeInsert()
+    @BeforeUpdate()
+    hashPassword() {
+        this.password = crypto.createHmac('sha256', this.password).digest('hex')
+    }
+
+    @Column({
+        length: 50,
+        unique: true,
+    })
+    @IsEmail()
+    email: string
+
+    @Column({})
+    isVerified: boolean
+
+    @Column({})
+    isActive: boolean
+
+    @BeforeInsert()
+    fillDefaults() {
+        this.isActive = true
+        this.isVerified = false
+    }
+
+    @CreateDateColumn({})
+    createdAt: Date
+
+    @UpdateDateColumn({})
+    updatedAt: Date
+}
