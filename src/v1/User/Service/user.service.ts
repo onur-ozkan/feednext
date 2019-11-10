676ca132c75f859c32d696acf325a1ef5a7999e0
ozkanonur <onurozkan.dev@outlook.com>
2019-11-10 11:47:30 +0300

    Serialize user data

--- src/v1/User/Service/user.service.ts
+++ src/v1/User/Service/user.service.ts
@@ -1,7 +1,8 @@
-import { Injectable, HttpException, HttpStatus } from '@nestjs/common'
+import { Injectable, NotFoundException } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { UsersEntity } from 'src/shared/Entities/users.entity'
 import { UsersRepository } from 'src/shared/Repositories/users.repository'
+import { OkException } from 'src/shared/Exceptions/ok.exception'
 
 @Injectable()
 export class UserService {
@@ -12,14 +13,21 @@ export class UserService {
     ) {}
 
     async getProfileByUsername(usernameParam: string): Promise<UsersEntity> {
-        let result: any
+        let result: UsersEntity
+        let id: string
         try {
             result = await this.usersRepository.findOneOrFail({
                 username: usernameParam,
             })
+            id = String(result['_id'])
+
+            delete result['_id']
+            delete result['password']
+            delete result['is_active']
+            delete result['is_verified']
         } catch (err) {
-            throw new HttpException(err, HttpStatus.NOT_FOUND)
+            throw new NotFoundException(`User with that username could not found in the database.`)
         }
-        throw new HttpException({statusCode: 200, message: `OK`, data: result }, HttpStatus.OK)
+        throw new OkException(`user_profile`, result, `User ${result.username} successfully loaded.`, id)
     }
 }
