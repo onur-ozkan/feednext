93d2af57d726f58c8591310fb0ecd0bda2fc4d79
ozkanonur <onurozkan.dev@outlook.com>
2019-11-10 23:28:35 +0300

    Create update endpoint for user module

--- src/v1/User/Controller/user.controller.ts
+++ src/v1/User/Controller/user.controller.ts
@@ -1,7 +1,9 @@
-import { Get, Param, Controller } from '@nestjs/common'
-import { ApiUseTags } from '@nestjs/swagger'
+import { Get, Param, Controller, Body, Post, UseGuards } from '@nestjs/common'
+import { ApiUseTags, ApiBearerAuth } from '@nestjs/swagger'
 import { UsersEntity } from 'src/shared/Entities/users.entity'
 import { UserService } from '../Service/user.service'
+import { UpdateUserDto } from '../Dto/update-user.dto'
+import { AuthGuard } from '@nestjs/passport'
 
 @ApiUseTags('v1/user')
 @Controller()
@@ -11,5 +13,12 @@ export class UsersController {
     @Get(':username')
     getProfileByUsername(@Param('username') username): Promise<UsersEntity> {
         return this.usersService.getProfileByUsername(username)
+    }
+
+    @ApiBearerAuth()
+    @UseGuards(AuthGuard('jwt'))
+    @Post(':username/update')
+    updateProfile(@Param('username') username, @Body() dto: UpdateUserDto ) {
+        return this.usersService.updateProfileByUsername(username, dto)
     }
 }
