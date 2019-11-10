a9152b53f32bc9bd663de7a5d24ce77b9b4e81e4
ozkanonur <onurozkan.dev@outlook.com>
2019-11-10 23:28:15 +0300

    Create updateProfileByUsername function

--- src/v1/User/Service/user.service.ts
+++ src/v1/User/Service/user.service.ts
@@ -1,8 +1,10 @@
-import { Injectable, NotFoundException } from '@nestjs/common'
+import { Injectable, NotFoundException, BadRequestException } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { UsersEntity } from 'src/shared/Entities/users.entity'
 import { UsersRepository } from 'src/shared/Repositories/users.repository'
 import { OkException } from 'src/shared/Exceptions/ok.exception'
+import { UpdateUserDto } from '../Dto/update-user.dto'
+import * as crypto from 'crypto'
 
 @Injectable()
 export class UserService {
@@ -13,21 +15,54 @@ export class UserService {
     ) {}
 
     async getProfileByUsername(usernameParam: string): Promise<UsersEntity> {
-        let result: UsersEntity
+        let profile: UsersEntity
         let id: string
         try {
-            result = await this.usersRepository.findOneOrFail({
+            profile = await this.usersRepository.findOneOrFail({
                 username: usernameParam,
             })
-            id = String(result['_id'])
+            id = String(profile['_id'])
 
-            delete result['_id']
-            delete result['password']
-            delete result['is_active']
-            delete result['is_verified']
         } catch (err) {
             throw new NotFoundException(`User with that username could not found in the database.`)
         }
-        throw new OkException(`user_profile`, result, `User ${result.username} successfully loaded.`, id)
+
+        delete profile['_id']
+        delete profile['password']
+        delete profile['is_active']
+        delete profile['is_verified']
+        throw new OkException(`user_profile`, profile, `User ${profile.username} is successfully loaded.`, id)
     }
+
+    async updateProfileByUsername(usernameParam: string, dto: UpdateUserDto): Promise<UsersEntity> {
+        const profile = await this.usersRepository.findOne({
+            username: usernameParam,
+        })
+
+        if (!profile) {
+            throw new NotFoundException(`User with that username could not found in the database.`)
+        }
+
+        const id = String(profile._id)
+
+        try {
+            if (dto.fullName) profile.full_name = dto.fullName
+            if (dto.email) profile.email = dto.email
+            if (dto.password) {
+                const hashedPassword = crypto.createHmac('sha256', dto.password).digest('hex')
+                profile.password = hashedPassword
+            }
+
+            await this.usersRepository.save(profile)
+        } catch (err) {
+            throw new BadRequestException(`The email that entered is duplicated in the database.`)
+        }
+
+        delete profile['_id']
+        delete profile['password']
+        delete profile['is_active']
+        delete profile['is_verified']
+        throw new OkException(`updated_profile`, profile, `User ${profile.username} is successfully updated.`, id)
+    }
+
 }
