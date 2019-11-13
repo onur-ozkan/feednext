8b2a355e6234cd69e2390936a6a6ccbe6de418dc
ozkanonur <onurozkan.dev@outlook.com>
2019-11-14 00:46:03 +0300

    Update: make user update endpoit as patch request

--- src/v1/User/Controller/user.controller.ts
+++ src/v1/User/Controller/user.controller.ts
@@ -1,4 +1,4 @@
-import { Get, Param, Controller, Body, Post, UseGuards } from '@nestjs/common'
+import { Get, Param, Controller, Body, Patch, UseGuards, Headers } from '@nestjs/common'
 import { ApiUseTags, ApiBearerAuth } from '@nestjs/swagger'
 import { UsersEntity } from 'src/shared/Entities/users.entity'
 import { UserService } from '../Service/user.service'
@@ -17,8 +17,13 @@ export class UsersController {
 
     @ApiBearerAuth()
     @UseGuards(AuthGuard('jwt'))
-    @Post(':username/update')
-    updateProfile(@Param('username') username, @Body() dto: UpdateUserDto ) {
+    @Patch(':username/update')
+    updateProfile(
+        @Param('username') username,
+        @Body() dto: UpdateUserDto,
+        @Headers('authorization') bearer: string,
+    ) {
+        // const authToken = bearer.substring(7)
         return this.usersService.updateProfileByUsername(username, dto)
     }
 }
