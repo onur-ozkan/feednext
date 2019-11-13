2f142564736636899a2e40f0659819a5bf8118b1
ozkanonur <onurozkan.dev@outlook.com>
2019-11-14 00:39:20 +0300

    Optimize blacklist middleware

--- src/shared/Middleware/blacklist.middleware.ts
+++ src/shared/Middleware/blacklist.middleware.ts
@@ -1,12 +1,11 @@
-import { Injectable, NestMiddleware, UnauthorizedException } from '@nestjs/common'
-import { FastifyRequest, FastifyReply } from 'fastify'
+import { Injectable, NestMiddleware, UnauthorizedException, Request, Response } from '@nestjs/common'
 import { RedisService } from '../Services/redis.service'
 
 @Injectable()
 export class BlacklistMiddleware implements NestMiddleware {
     constructor(private readonly redisService: RedisService) {}
 
-    async use(req: FastifyRequest, _res: FastifyReply<Response>, next: Function) {
+    async use(@Request() req, @Response() _res, next: Function) {
         const token = req.headers.authorization.substring(7)
         const isTokenDead = await this.redisService.getData(token)
 
