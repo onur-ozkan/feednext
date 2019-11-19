10a606305a48e25214db311b035cc0278b64d1f8
Onur Özkan <onurozkan.dev@outlook.com>
2019-11-20 01:01:04 +0300

    Add role guard to create-category endpoint

--- src/v1/Category/Controller/category.controller.ts
+++ src/v1/Category/Controller/category.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Post, Body, HttpException, UseGuards } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
 import { ApiUseTags, ApiBearerAuth } from '@nestjs/swagger'
+import { RolesGuard } from 'src/shared/Guards/roles.guard'
+import { Roles } from 'src/shared/Decorators/roles.decorator'
 import { CreateCategoryDto } from '../Dto/create-category.dto'
 import { CategoryService } from '../Service/category.service'
 
 @ApiUseTags('v1/category')
 @Controller()
+@UseGuards(RolesGuard)
 export class CategoryController {
     constructor(
         private readonly categoryService: CategoryService,
@@ -14,6 +17,7 @@ export class CategoryController {
     @ApiBearerAuth()
     @UseGuards(AuthGuard('jwt'))
     @Post('create-category')
+    @Roles(3)
     createCategory(@Body() dto: CreateCategoryDto): Promise<HttpException> {
         return this.categoryService.createCategory(dto)
     }
