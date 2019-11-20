d8379eee1394d1233352c31b8395f86e11f9aa37
Onur Özkan <onurozkan.dev@outlook.com>
2019-11-20 11:13:04 +0300

    Throw exception if token doesnt exist

--- src/shared/Services/current-user.service.ts
+++ src/shared/Services/current-user.service.ts
@@ -1,7 +1,9 @@
+import { UnauthorizedException } from '@nestjs/common'
 import * as jwt from 'jsonwebtoken'
 
 export class CurrentUserService {
     public getCurrentUser(token: string, property: string): any {
+        if (!token) throw new UnauthorizedException()
         const authData: any = jwt.decode(token.substring(7))
 
         switch (property) {
