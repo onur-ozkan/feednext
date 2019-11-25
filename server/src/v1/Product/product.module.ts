cb439f3b1bd3120da6fbc040391740112a296c02
ozkanonur <onurozkan.dev@outlook.com>
2019-11-25 16:12:21 +0300

    Configure product module

--- server/src/v1/Product/product.module.ts
+++ server/src/v1/Product/product.module.ts
@@ -1,6 +1,16 @@
 import { Module } from '@nestjs/common'
+import { ProductsEntity } from 'src/shared/Entities/products.entity'
+import { ProductsRepository } from 'src/shared/Repositories/products.repository'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ProductService } from './Service/product.service'
+import { ProductController } from './Controller/product.controller'
+import { CategoriesRepository } from 'src/shared/Repositories/categories.repository'
 
 @Module({
-    imports: [],
+    imports: [TypeOrmModule.forFeature([ProductsEntity, ProductsRepository, CategoriesRepository])],
+    providers: [ProductService],
+    exports: [ProductService],
+    controllers: [ProductController],
 })
+
 export class ProductModule {}
