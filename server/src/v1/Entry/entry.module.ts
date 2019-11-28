7aab3724cb091e0c71e433af5178bdf9cc6c1752
ozkanonur <onurozkan.dev@outlook.com>
2019-11-28 19:54:00 +0300

    Add ProductsRepository in Entry Module

--- server/src/v1/Entry/entry.module.ts
+++ server/src/v1/Entry/entry.module.ts
@@ -4,9 +4,10 @@ import { EntriesEntity } from 'src/shared/Entities/entries.entity'
 import { EntriesRepository } from 'src/shared/Repositories/entries.repository'
 import { EntryService } from './Service/entry.service'
 import { EntryController } from './Controller/entry.controller'
+import { ProductsRepository } from 'src/shared/Repositories/products.repository'
 
 @Module({
-    imports: [TypeOrmModule.forFeature([EntriesEntity, EntriesRepository])],
+    imports: [TypeOrmModule.forFeature([EntriesEntity, EntriesRepository, ProductsRepository])],
     providers: [EntryService],
     exports: [EntryService],
     controllers: [EntryController],
