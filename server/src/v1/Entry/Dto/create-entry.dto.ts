ec3aaac67525b347b2cb7c2ff24107bf3e4ed16d
ozkanonur <onurozkan.dev@outlook.com>
2019-11-28 19:24:50 +0300

    Create 'create-entry' dto

---
+++ server/src/v1/Entry/Dto/create-entry.dto.ts
@@ -0,0 +1,18 @@
+import { ApiModelProperty } from '@nestjs/swagger'
+import { IsNotEmpty, IsMongoId } from 'class-validator'
+
+export class CreateEntryDto {
+    @ApiModelProperty({
+        required: true,
+        example: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    })
+    @IsNotEmpty()
+    text: string
+
+    @ApiModelProperty({
+        required: true,
+        example: '507f1f77bcf86cd799439011',
+    })
+    @IsMongoId()
+    productId: string
+}
