ee5f6554c37faf85225e068667832690d9694f6f
ozkanonur <onurozkan.dev@outlook.com>
2019-11-28 19:12:44 +0300

    Configure entry module up

--- server/src/v1/Entry/entry.module.ts
+++ server/src/v1/Entry/entry.module.ts
@@ -1,6 +1,15 @@
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { EntriesEntity } from 'src/shared/Entities/entries.entity'
+import { EntriesRepository } from 'src/shared/Repositories/entries.repository'
+import { EntryService } from './Service/entry.service'
+import { EntryController } from './Controller/entry.controller'
 
 @Module({
-    imports: [],
+    imports: [TypeOrmModule.forFeature([EntriesEntity, EntriesRepository])],
+    providers: [EntryService],
+    exports: [EntryService],
+    controllers: [EntryController],
 })
+
 export class EntryModule {}
