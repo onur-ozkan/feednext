5c8686073463b1d16223b6791bb98b5f8b2a7df8
ozkanonur <onurozkan.dev@outlook.com>
2019-12-02 21:16:19 +0300

    Create 2 more ports of new instances

--- server/pm2.config.js
+++ server/pm2.config.js
@@ -21,6 +21,26 @@ module.exports = {
         NODE_ENV: "production",
         PORT: process.env.APP_PORT + 1
       }
+    },
+    {
+      name: "api_3",
+      script: "./dist/main.js",
+      instances: 1,
+      exec_mode: "cluster",
+      env: {
+        NODE_ENV: "production",
+        PORT: process.env.APP_PORT + 2
+      }
+    },
+    {
+      name: "api_4",
+      script: "./dist/main.js",
+      instances: 1,
+      exec_mode: "cluster",
+      env: {
+        NODE_ENV: "production",
+        PORT: process.env.APP_PORT + 3
+      }
     }
   ]
 }
