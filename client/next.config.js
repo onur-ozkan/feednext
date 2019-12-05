1c53c8fffb9ff8a37b3e7dba8c58be662d42a9fd
Onur Özkan <onurozkan.dev@outlook.com>
2019-12-05 16:15:11 +0300

    Re-configure next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -1,17 +1,18 @@
-const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer')
-const withCSS = require('@zeit/next-css')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+const withLess = require('@zeit/next-less')
 
-module.exports = withCSS(
-  {
+module.exports = withLess({
+    lessLoaderOptions: {
+        javascriptEnabled: true
+    },
     webpack(config) {
-      if (process.env.ANALYZE) {
-        config.plugins.push(new BundleAnalyzerPlugin({
-          analyzerMode: 'server',
-          analyzerPort: 8888,
-          openAnalyzer: true
-        }))
-      }
-      return config
+        if (process.env.ANALYZE) {
+            config.plugins.push(new BundleAnalyzerPlugin({
+                analyzerMode: 'server',
+                analyzerPort: 8888,
+                openAnalyzer: true
+            }))
+        }
+        return config
     }
-  }
-)
+})
