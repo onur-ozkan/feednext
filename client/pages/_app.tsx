c3940f611bdb6f914137ea901c78cf8785577efc
Onur Özkan <onurozkan.dev@outlook.com>
2019-12-05 16:18:24 +0300

    Optimize _app.tsx

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,37 +1,37 @@
+import React from 'react'
+import App from 'next/app'
+import { Provider } from 'react-redux'
+import { getStore } from '../src/store'
 import 'isomorphic-fetch'
-import * as React from 'react'
-import {Provider} from 'react-redux'
-import App, {Container} from 'next/app'
-import {getStore} from '../src/store'
-import 'antd/dist/antd.css'
+import '../public/main.less'
 
 export default class extends App {
-  static async getInitialProps({Component, router, ctx}) {
-    const server = !!ctx.req
-    const store = getStore(undefined, server)
-    const state = store.getState()
-    const out = {state, server} as any
+    static async getInitialProps({Component, router, ctx}) {
+        const server = !!ctx.req
+        const store = getStore(undefined, server)
+        const state = store.getState()
+        const out = {state, server} as any
 
-    if (Component.getInitialProps) {
-      return {
-        ...out,
-        pageProps: {  
-          ...await Component.getInitialProps(ctx)
+        if (Component.getInitialProps) {
+            return {
+                ...out,
+                pageProps: {
+                    ...await Component.getInitialProps(ctx)
+                }
+            }
         }
-      }
+
+        return out
     }
 
-    return out
-  }
+    render() {
+        const { props } = this as any
+        const { Component, pageProps } = props
 
-  render() {
-    const {props} = this as any
-    const {Component, pageProps} = props
-
-    return (
-      <Provider store={getStore(undefined, props.server)}>
-          <Component {...pageProps} />
-      </Provider>
-    )
-  }
+        return (
+            <Provider store={getStore(undefined, props.server)}>
+                <Component {...pageProps} />
+            </Provider>
+        )
+    }
 }
