377070b2a791fa3467d9c2362654619eb102b6fa
Onur Özkan <onurozkan.dev@outlook.com>
2019-12-06 18:45:54 +0300

    Create Sign-in page and component

---
+++ client/pages/sign-in.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import SignInComponent from '../src/components/Signin/Sign-in'
+
+export default props =>
+  <SignInComponent />
---
+++ client/src/components/Signin/Sign-in.tsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { FormComponentProps } from 'antd/lib/form/Form'
+import { Form, Icon, Input, Button, Checkbox, Row } from 'antd';
+
+const SignInComponent: React.FC<FormComponentProps> = props => {
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values)
+            }
+        })
+    }
+
+    const { getFieldDecorator } = props.form
+
+    return (
+        <Row type="flex" justify="center" align="middle" style={{minHeight: '100vh'}}>
+            <Form  style={{maxWidth: '300px'}} onSubmit={handleSubmit} className="login-form">
+                <Form.Item>
+                    {getFieldDecorator('username', {
+                        rules: [{ required: true, message: 'Please input your username!' }],
+                    })(
+                        <Input
+                            prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                            placeholder="Username"
+                        />,
+                    )}
+                </Form.Item>
+                <Form.Item>
+                    {getFieldDecorator('password', {
+                        rules: [{ required: true, message: 'Please input your Password!' }],
+                    })(
+                        <Input
+                            prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                            type="password"
+                            placeholder="Password"
+                        />,
+                    )}
+                </Form.Item>
+                <Form.Item>
+                    {getFieldDecorator('remember', {
+                        valuePropName: 'checked',
+                        initialValue: true,
+                    })(<Checkbox>Remember me</Checkbox>)}
+                    <a style={{float: 'right'}} className="login-form-forgot" href="">
+                        Forgot password
+                    </a>
+                    <Button style={{width: '100%'}} type="primary" htmlType="submit" className="login-form-button">
+                        Log in
+                    </Button>
+                    Or <a href="">register now!</a>
+                </Form.Item>
+            </Form>
+        </Row>
+    )
+}
+
+export default Form.create({ name: 'normal_login' })(SignInComponent)
