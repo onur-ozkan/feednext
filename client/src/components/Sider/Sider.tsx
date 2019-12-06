132c6decef41821e7543f4f2a3ff0630f2f23deb
Onur Özkan <onurozkan.dev@outlook.com>
2019-12-06 12:33:01 +0300

    Conditional rendering in Sider

--- client/src/components/Sider/Sider.tsx
+++ client/src/components/Sider/Sider.tsx
@@ -10,47 +10,26 @@ export const SiderComponent: React.FunctionComponent = () => {
 
     const handleChange = (): void => setCollapsed(!collapsed)
 
+    const products = () => {
+        if (!collapsed) {
+            return (
+                <Menu defaultSelectedKeys={['1']} mode="inline">
+                    <Menu.Item key="1">
+                        <Icon type="form" />
+                        <span> Product Example </span>
+                    </Menu.Item>
+                    <Menu.Item key="2">
+                        <Icon type="form" />
+                        <span> Product Exampleasd </span>
+                    </Menu.Item>
+                </Menu>
+            )
+        }
+    }
+
     return (
-        <Sider theme="light" collapsible collapsed={collapsed} onCollapse={handleChange}>
-            <Menu defaultSelectedKeys={['1']} mode="inline">
-                <Menu.Item key="1">
-                    <Icon type="pie-chart" />
-                    <span> Option 1 </span>
-                </Menu.Item>
-                <Menu.Item key="2">
-                    <Icon type="desktop" />
-                    <span>Option 2</span>
-                </Menu.Item>
-                <SubMenu
-                    key="sub1"
-                    title={
-                        <span>
-                            <Icon type="user" />
-                            <span> User </span>
-                        </span>
-                    }
-                >
-                    <Menu.Item key="3">Tom</Menu.Item>
-                    <Menu.Item key="4">Bill</Menu.Item>
-                    <Menu.Item key="5">Alex</Menu.Item>
-                </SubMenu>
-                <SubMenu
-                    key="sub2"
-                    title={
-                    <span>
-                    <Icon type="team" />
-                    <span>Team</span>
-                    </span>
-                    }
-                >
-                    <Menu.Item key="6"> Team 1 </Menu.Item>
-                    <Menu.Item key="8"> Team 2 </Menu.Item>
-                </SubMenu>
-                <Menu.Item key="9">
-                    <Icon type="file" />
-                    <span> File </span>
-                </Menu.Item>
-            </Menu>
+        <Sider style={{  overflowY: 'scroll', height: '89vh' }} theme="light" collapsible width={'250'} collapsed={collapsed} onCollapse={handleChange}>
+            {products()}
         </Sider>
     )
 }
