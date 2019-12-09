66862073a48b72d16a5b5490da868d8473d5d0c9
Onur Özkan <onurozkan.dev@outlook.com>
2019-12-09 04:02:04 +0300

    [client] Add search input to inside of the sider

--- client/src/components/Sider/Sider.tsx
+++ client/src/components/Sider/Sider.tsx
@@ -1,21 +1,31 @@
 import React, { useState, Dispatch, SetStateAction } from 'react'
-import { Layout, Menu, Icon } from 'antd'
+import { Layout, Menu, Icon, Input } from 'antd'
 
 const { Sider } = Layout
 
 export const SiderComponent: React.FunctionComponent = () => {
-
     const [collapsed, setCollapsed]: [boolean, Dispatch<SetStateAction<boolean>>] = useState(false)
+    const [isSearching, setIsSearching]: [boolean, Dispatch<SetStateAction<boolean>>] = useState(false)
 
     const handleChange = (): void => setCollapsed(!collapsed)
 
     const products = () => {
         if (!collapsed) {
             return (
-                <Menu mode="inline">
+                <Menu mode="inline" style={{ padding: '5px' }}>
+                    <div style={{ textAlign: 'center', marginBottom: '-35px', zIndex: -1 }}>
+                        <Input.Search
+                            loading={isSearching}
+                            placeholder="Search a feed title"
+                            onSearch={(value) => console.log(value)}
+                            style={{ width: 200, zIndex: 1 }}
+                        />
+                    </div>
+
+                    <Menu.Item style={{ backgroundColor: 'transparent' }} />
                     <Menu.Item key="1">
                         <Icon type="form" />
-                        <span> Product Example </span>
+                        <span> Example Title </span>
                     </Menu.Item>
                     <Menu.Item key="2">
                         <Icon type="form" />
@@ -27,7 +37,14 @@ export const SiderComponent: React.FunctionComponent = () => {
     }
 
     return (
-        <Sider style={{  overflowY: 'scroll', height: '89vh' }} theme="light" collapsible width={'250'} collapsed={collapsed} onCollapse={handleChange}>
+        <Sider
+            style={{ overflowY: 'scroll', height: '89vh' }}
+            theme="light"
+            collapsible
+            width={'250'}
+            collapsed={collapsed}
+            onCollapse={handleChange}
+        >
             {products()}
         </Sider>
     )
