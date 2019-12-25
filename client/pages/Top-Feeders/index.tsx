092aaf5460a03cf81447af86f6d7fe15f2f5c311
ozkanonur <onurozkan.dev@outlook.com>
2019-12-25 23:54:40 +0300

    Create top-feeders page template

--- client/pages/Top-Feeders/index.tsx
+++ client/pages/Top-Feeders/index.tsx
@@ -1,8 +1,45 @@
 import * as React from 'react'
+import { Tabs, Table } from 'antd'
 import { LayoutComponent } from '../../src/components/Layout/Layout'
+
+const columns = [
+    {
+        title: 'Name',
+        dataIndex: 'name',
+        width: 150
+    },
+    {
+        title: 'Age',
+        dataIndex: 'age',
+        width: 150
+    },
+    {
+        title: 'Address',
+        dataIndex: 'address'
+    }
+]
+
+const data = []
+for (let i = 0; i < 100; i++) {
+    data.push({
+        key: i,
+        name: `Edward King ${i}`,
+        age: 32,
+        address: `London, Park Lane no. ${i}`
+    })
+}
 
 export default (props) => (
     <LayoutComponent>
-        <h1>Top Feeders will handle this page</h1>
+        <span style={{ fontSize: '25px', color: '#212121' }}> Top Feeders </span>
+        <br />
+        <Tabs defaultActiveKey="1">
+            <Tabs.TabPane tab="Global" key="1">
+                <Table columns={columns} dataSource={data} pagination={{ pageSize: 50 }} scroll={{ y: 750 }} />
+            </Tabs.TabPane>
+            <Tabs.TabPane tab="By Country" key="2">
+                Content of Tab Pane 2
+			</Tabs.TabPane>
+        </Tabs>
     </LayoutComponent>
 )
