0e81167539d4e6ffa1213e294c2333dcb470c211
ozkanonur <onurozkan.dev@outlook.com>
2020-01-29 10:26:33 +0300

    [client] Convert Sign class to function component

--- client/pages/Sign/index.tsx
+++ client/pages/Sign/index.tsx
@@ -1,60 +1,54 @@
-import React from 'react'
+// Global dependencies
+import React, { useState } from 'react'
 import Link from 'next/link'
+import { Icon, Row, Menu } from 'antd'
+
+// Local files
 import SignInComponent from '../../src/components/Signin/Signin'
 import SignUpComponent from '../../src/components/Signup/Signup'
 
-import { Icon, Row, Menu } from 'antd'
 
-interface IComponentState {
-    isSignIn: boolean
-}
+// export default class Sign extends React.Component<{}, IComponentState> 
+const Sign = props => {
+    const [isSignIn, setIsSignIn] = useState(true)
 
-export default class Sign extends React.Component<{}, IComponentState> {
-    constructor(props) {
-        super(props)
-        this.state = {
-            isSignIn: true
-        }
-    }
-
-    private handleSignScreens = () => {
-        if (this.state.isSignIn) {
+    const handleSignScreens = () => {
+        if (isSignIn) {
             return <SignInComponent />
-        } else {
-            return <SignUpComponent />
         }
+        return <SignUpComponent />
     }
 
-    render() {
-        return (
-            <div>
-                <Menu
-                    defaultSelectedKeys={['signin']}
-                    style={{ display: 'flex', justifyContent: 'center', padding: '5px', border: 0 }}
-                    mode="horizontal"
-                >
-                    <Menu.Item key="exit">
-                        <Link href="/">
-                            <a>
-                                <Icon type="left-circle" />
-                                Back to App
-							</a>
-                        </Link>
-                    </Menu.Item>
-                    <hr />
-                    <Menu.Item onClick={() => this.setState({ isSignIn: false })} key="signup">
-                        <Icon type="user-add" />
-                        Sign Up
-					</Menu.Item>
-                    <Menu.Item onClick={() => this.setState({ isSignIn: true })} key="signin">
-                        <Icon type="login" />
-                        Sign In
-					</Menu.Item>
-                </Menu>
-                <Row type="flex" justify="center" align="middle" style={{ minHeight: '100vh' }}>
-                    {this.handleSignScreens()}
-                </Row>
-            </div>
-        )
-    }
+    return (
+        <div>
+            <Menu
+                defaultSelectedKeys={['signin']}
+                style={{ display: 'flex', justifyContent: 'center', padding: '5px', border: 0 }}
+                mode="horizontal"
+            >
+                <Menu.Item key="exit">
+                    <Link href="/">
+                        <a>
+                            <Icon type="left-circle" />
+                            Back to App
+                        </a>
+                    </Link>
+                </Menu.Item>
+                <hr />
+                <Menu.Item onClick={() => setIsSignIn(false)} key="signup">
+                    <Icon type="user-add" />
+                    Sign Up
+                </Menu.Item>
+                <Menu.Item onClick={() => setIsSignIn(true)} key="signin">
+                    <Icon type="login" />
+                    Sign In
+                </Menu.Item>
+            </Menu>
+            <Row type="flex" justify="center" align="middle" style={{ minHeight: '100vh' }}>
+                {handleSignScreens()}
+            </Row>
+        </div>
+    )
 }
+
+export default Sign
