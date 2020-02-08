bc80484bbe021b4dfbcc2cf46381903694ac7985
ozkanonur <onurozkan.dev@outlook.com>
2020-02-08 17:37:17 +0300

    [client] Define variable types

--- client/src/constants/env.ts
+++ client/src/constants/env.ts
@@ -1,12 +1,12 @@
-export const DEV = process.env.NODE_ENV !== 'production'
+export const DEV: boolean = process.env.NODE_ENV !== 'production'
 
-export const API_URL='http://localhost/api/v1/'
+export const API_URL: string = 'http://localhost/api/v1/'
 
-export const GA_TRACKING_ID = ''
-export const FB_TRACKING_ID = ''
-export const SENTRY_TRACKING_ID = ''
+export const GA_TRACKING_ID: string = ''
+export const FB_TRACKING_ID: string = ''
+export const SENTRY_TRACKING_ID: string = ''
 
-export const SITE_NAME = ''
-export const SITE_TITLE = ''
-export const SITE_DESCRIPTION = ''
-export const SITE_IMAGE = ''
+export const SITE_NAME: string = ''
+export const SITE_TITLE: string = ''
+export const SITE_DESCRIPTION: string = ''
+export const SITE_IMAGE: string = ''
