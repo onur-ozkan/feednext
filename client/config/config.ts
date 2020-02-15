32a9d58aa4e3fdd54447c3148bb1d73abceaea8a
ozkanonur <onurozkan.dev@outlook.com>
2020-02-16 00:00:07 +0300

    [client] Add new endpoint 'register'

--- client/config/config.ts
+++ client/config/config.ts
@@ -81,9 +81,29 @@ export default {
       component: '../layouts/UserLayout',
       routes: [
         {
+          path: '/user',
+          redirect: '/user/login',
+        },
+        {
           name: 'login',
+          icon: 'smile',
           path: '/user/login',
           component: './user/login',
+        },
+        {
+          name: 'register-result',
+          icon: 'smile',
+          path: '/user/register-result',
+          component: './user/register-result',
+        },
+        {
+          name: 'register',
+          icon: 'smile',
+          path: '/user/register',
+          component: './user/register',
+        },
+        {
+          component: '404',
         },
       ],
     },
@@ -162,7 +182,31 @@ export default {
   ],
   // Theme for antd: https://ant.design/docs/react/customize-theme-cn
   theme: {
-    // ...darkTheme,
+    '@primary-color': '#ff2d20',
+    // primary color for all components
+    '@link-color': '#1890ff',
+    // link color
+    '@success-color': '#52c41a',
+    // success state color
+    '@warning-color': '#faad14',
+    // warning state color
+    '@error-color': '#d60d17',
+    // error state color
+    '@font-size-base': '14px',
+    // major text font size
+    '@heading-color': 'rgba(0, 0, 0, 0.85)',
+    // heading text color
+    '@text-color': 'rgba(0, 0, 0, 0.65)',
+    // major text color
+    '@text-color-secondary': 'rgba(0, 0, 0, .45)',
+    // secondary text color
+    '@disabled-color': 'rgba(0, 0, 0, .25)',
+    // disable state color
+    '@border-radius-base': '4px',
+    // major border radius
+    '@border-color-base': '#d9d9d9',
+    // major border color
+    '@box-shadow-base': '0 2px 8px rgba(0, 0, 0, 0.15)', // major shadow for layers
   },
   define: {
     REACT_APP_ENV: REACT_APP_ENV || false,
