f7d974e4d23a03e5adfe1045e5bfebe6a52aa5cc
ozkanonur <onurozkan.dev@outlook.com>
2020-03-27 15:13:39 +0300

    [client] Refactor React Class to FC and dispatch signed user

--- client/src/pages/Auth/Login/index.tsx
+++ client/src/pages/Auth/Login/index.tsx
@@ -1,177 +1,128 @@
-import { Alert, Checkbox } from 'antd'
+import { Checkbox, message } from 'antd'
 import { FormattedMessage, formatMessage } from 'umi-plugin-react/locale'
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { CheckboxChangeEvent } from 'antd/es/checkbox'
-import { Dispatch } from 'redux'
 import { FormComponentProps } from '@ant-design/compatible/es/form'
 import Link from 'umi/link'
-import { connect } from 'dva'
-import { StateType } from './model'
+import { useDispatch, useSelector } from 'react-redux'
 import LoginComponents from './components/Login'
 import styles from './style.less'
 import api from '@/utils/api.ts'
+import { StartUserActions } from '@/redux/Actions/User'
 
 const { Tab, UserName, Password, Submit } = LoginComponents
 
-declare interface LoginProps {
-	dispatch: Dispatch<any>
-	userAndlogin: StateType
-	submitting: boolean
-}
-declare interface LoginState {
-	type: string
-	autoLogin: boolean
-}
 export declare interface FormDataType {
-	username: string
+	usernameOrEmail: string
 	password: string
 }
 
-class Login extends Component<LoginProps, LoginState> {
-	loginForm: FormComponentProps['form'] | undefined | null = undefined
+const Login: React.FunctionComponent = () => {
+	let loginForm: FormComponentProps['form'] | undefined | null = undefined
 
-	state: LoginState = {
-		type: 'account',
-		autoLogin: true,
+	const [autoLogin, setAutoLogin] = useState(true)
+	const user = useSelector((state: any) => state.user)
+	const dispatch = useDispatch()
+
+	useEffect(() => {
+		if (user) console.log(user)
+	}, [user])
+
+	const changeAutoLogin = (e: CheckboxChangeEvent): void => {
+		setAutoLogin(e.target.checked)
 	}
 
-	changeAutoLogin = (e: CheckboxChangeEvent): void => {
-		this.setState({
-			autoLogin: e.target.checked,
-		})
-	}
+	const handleSignIn = (err: any, values: FormDataType): void => {
+		if (err) return
+		const isEmail = /\S+@\S+\.\S+/.test(values.usernameOrEmail)
 
-	handleSubmit = (err: any, values: FormDataType): void => {
 		api.signIn({
-			email: values.username,
+			...(isEmail ? { email: values.usernameOrEmail } : { username: values.usernameOrEmail }),
 			password: values.password,
 		})
-			.then(value => console.log(value))
-			.catch(err => console.log(err.response))
-		const { type } = this.state
-		if (!err) {
-			const { dispatch } = this.props
-			dispatch({
-				type: 'userAndlogin/login',
-				payload: {
-					...values,
-					type,
-				},
+			.then(res => {
+				dispatch(StartUserActions.SignIn({ userInformation: res.data.data }))
 			})
-		}
+			.catch(error => {
+				message.error(error.response.data.message, 5)
+			})
 	}
 
-	onTabChange = (type: string): void => {
-		this.setState({
-			type,
-		})
-	}
-
-	renderMessage = (content: string): JSX.Element => (
-		<Alert
-			style={{
-				marginBottom: 24,
-			}}
-			message={content}
-			type="error"
-			showIcon
-		/>
-	)
-
-	render(): JSX.Element {
-		const { userAndlogin, submitting } = this.props
-		const { status, type: loginType } = userAndlogin
-		const { type, autoLogin } = this.state
-		return (
-			<div className={styles.main}>
-				<LoginComponents
-					defaultActiveKey={type}
-					onTabChange={this.onTabChange}
-					onSubmit={this.handleSubmit}
-					ref={(form: any): void => {
-						this.loginForm = form
-					}}
+	return (
+		<div className={styles.main}>
+			<LoginComponents
+				defaultActiveKey="sign-in"
+				onSubmit={handleSignIn}
+				ref={(form: any): void => {
+					loginForm = form
+				}}
+			>
+				<Tab
+					key="sign-in"
+					tab={formatMessage({
+						id: 'userandlogin.login.tab-login-signin',
+					})}
 				>
-					<Tab
-						key="account"
-						tab={formatMessage({
-							id: 'userandlogin.login.tab-login-signin',
-						})}
-					>
-						{status === 'error' &&
-							loginType === 'account' &&
-							!submitting &&
-							this.renderMessage(
-								formatMessage({
-									id: 'userandlogin.login.message-invalid-credentials',
+					<UserName
+						name="usernameOrEmail"
+						placeholder={`${formatMessage({
+							id: 'userandlogin.login.userName',
+						})}: admin or user`}
+						rules={[
+							{
+								required: true,
+								message: formatMessage({
+									id: 'userandlogin.userName.required',
 								}),
-							)}
-						<UserName
-							name="username"
-							placeholder={`${formatMessage({
-								id: 'userandlogin.login.userName',
-							})}: admin or user`}
-							rules={[
-								{
-									required: true,
-									message: formatMessage({
-										id: 'userandlogin.userName.required',
-									}),
-								},
-							]}
-						/>
-						<Password
-							name="password"
-							placeholder={`${formatMessage({
-								id: 'userandlogin.login.password',
-							})}: ant.design`}
-							rules={[
-								{
-									required: true,
-									message: formatMessage({
-										id: 'userandlogin.password.required',
-									}),
-								},
-							]}
-							onPressEnter={(e): void => {
-								e.preventDefault()
-								if (this.loginForm) {
-									this.loginForm.validateFields(this.handleSubmit)
-								}
-							}}
-						/>
-					</Tab>
-					<div>
-						<Checkbox checked={autoLogin} onChange={this.changeAutoLogin}>
-							<FormattedMessage id="userandlogin.login.remember-me" />
-						</Checkbox>
-						<a
-							style={{
-								float: 'right',
-							}}
-							href=""
-						>
-							<FormattedMessage id="userandlogin.login.forgot-password" />
-						</a>
-					</div>
-					<Submit loading={submitting}>
-						<FormattedMessage id="userandlogin.login.signin" />
-					</Submit>
-					<div className={styles.other}>
-						<Link className={styles.register} to="/auth/sign-up">
-							<FormattedMessage id="userandlogin.login.signup" />
-						</Link>
-					</div>
-				</LoginComponents>
-			</div>
-		)
-	}
+							},
+						]}
+					/>
+					<Password
+						name="password"
+						placeholder={`${formatMessage({
+							id: 'userandlogin.login.password',
+						})}: ant.design`}
+						rules={[
+							{
+								required: true,
+								message: formatMessage({
+									id: 'userandlogin.password.required',
+								}),
+							},
+						]}
+						onPressEnter={(e): void => {
+							e.preventDefault()
+							if (loginForm) {
+								loginForm.validateFields(handleSignIn)
+							}
+						}}
+					/>
+				</Tab>
+				<div>
+					<Checkbox checked={autoLogin} onChange={changeAutoLogin}>
+						<FormattedMessage id="userandlogin.login.remember-me" />
+					</Checkbox>
+					<a
+						style={{
+							float: 'right',
+						}}
+						href=""
+					>
+						<FormattedMessage id="userandlogin.login.forgot-password" />
+					</a>
+				</div>
+				<Submit>
+					<FormattedMessage id="userandlogin.login.signin" />
+				</Submit>
+				<div className={styles.other}>
+					<Link className={styles.register} to="/auth/sign-up">
+						<FormattedMessage id="userandlogin.login.signup" />
+					</Link>
+				</div>
+			</LoginComponents>
+		</div>
+	)
 }
 
-export default connect(
-	({ userAndlogin, loading }: { userAndlogin: StateType; loading: { effects: { [key: string]: boolean } } }) => ({
-		userAndlogin,
-		submitting: loading.effects['userAndlogin/login'],
-	}),
-)(Login)
+export default Login
