2a5caf8b0d72934eb1a08e52fafa5bdd852b14ed
ozkanonur <onurozkan.dev@outlook.com>
2020-04-01 19:05:00 +0300

    [client] Add new requests in api service

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -6,4 +6,10 @@ axios.defaults.baseURL = getConstant('API_URL')
 export default {
 	signIn: (obj: { username?: string; email?: string; password: string }): Promise<AxiosResponse> =>
 		axios.post('/v1/auth/signin', obj),
+	signUp: (obj: { fullName: string; username: string; email: string; password: string }): Promise<AxiosResponse> =>
+		axios.post('/v1/auth/signup', obj),
+	fetchAllFeeds: (): Promise<AxiosResponse> => axios.get('/v1/title/all?limit=5'),
+	fetchFeaturedEntryByTitleId: (titleId: string): Promise<AxiosResponse> => axios.get(`/v1/entry/${titleId}/featured`),
+	fetchEntriesByTitleId: (titleId: string): Promise<AxiosResponse> => axios.get(`/v1/entry/${titleId}/all?limit=5`),
+	fetchOneCategoryById: (categoryId: string): Promise<AxiosResponse> => axios.get(`/v1/category/${categoryId}`),
 }
