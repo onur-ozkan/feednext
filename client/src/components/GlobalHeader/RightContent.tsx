fc0a2fea8a76cccf719e080aa2a610a0669584d6
ozkanonur <onurozkan.dev@outlook.com>
2020-04-11 19:57:04 +0300

    [client] Render RightContent component if user is signed in

--- client/src/components/GlobalHeader/RightContent.tsx
+++ client/src/components/GlobalHeader/RightContent.tsx
@@ -1,9 +1,7 @@
 import { Tag, Button } from 'antd'
 import React from 'react'
-import { connect } from 'dva'
 import { formatMessage } from 'umi-plugin-react/locale'
-import { ConnectProps, ConnectState } from '@/models/connect'
-import { PlusCircleOutlined } from '@ant-design/icons'
+import { PlusCircleOutlined, LoginOutlined } from '@ant-design/icons'
 
 import Avatar from './AvatarDropdown'
 import HeaderSearch from '../HeaderSearch'
@@ -11,9 +9,10 @@ import SelectLang from '../SelectLang'
 import styles from './index.less'
 import NoticeIconView from './NoticeIconView'
 import { router } from 'umi'
+import { useSelector } from 'react-redux'
 
 export type SiderTheme = 'light' | 'dark'
-export declare interface GlobalHeaderRightProps extends ConnectProps {
+export declare interface GlobalHeaderRightProps {
 	theme?: SiderTheme
 	layout: 'sidemenu' | 'topmenu'
 }
@@ -27,6 +26,7 @@ const ENVTagColor = {
 const GlobalHeaderRight: React.SFC<GlobalHeaderRightProps> = props => {
 	const { theme, layout } = props
 	let className = styles.right
+	const user = useSelector((state: any) => state.user)
 
 	if (theme === 'dark' && layout === 'topmenu') {
 		className = `${styles.right}  ${styles.dark}`
@@ -36,17 +36,33 @@ const GlobalHeaderRight: React.SFC<GlobalHeaderRightProps> = props => {
 		router.push(path)
 	}
 
+	const handleAuthorizedElements = (): JSX.Element => {
+		if (user) {
+			return (
+				<>
+					<Button
+						onClick={(): void => routeTo('/feeds/create-feed')}
+						type="dashed"
+						shape="round"
+						icon={<PlusCircleOutlined />}
+					>
+						New Feed
+					</Button>
+					<NoticeIconView />
+					<Avatar />
+				</>
+			)
+		}
+
+		return (
+			<Button onClick={(): void => routeTo('/auth/sign-in')} type="primary" shape="round" icon={<LoginOutlined />}>
+				Sign In
+			</Button>
+		)
+	}
+
 	return (
 		<div className={className}>
-			<Button
-				onClick={(): void => routeTo('/feeds/create-feed')}
-				type="dashed"
-				shape="round"
-				icon={<PlusCircleOutlined />}
-			>
-				{' '}
-				New Feed{' '}
-			</Button>
 			<HeaderSearch
 				className={`${styles.action} ${styles.search}`}
 				placeholder={formatMessage({
@@ -71,15 +87,11 @@ const GlobalHeaderRight: React.SFC<GlobalHeaderRightProps> = props => {
 					return
 				}}
 			/>
-			<NoticeIconView />
-			<Avatar />
+			{handleAuthorizedElements()}
 			<SelectLang className={styles.action} />
 			{REACT_APP_ENV && <Tag color={ENVTagColor[REACT_APP_ENV]}>{REACT_APP_ENV}</Tag>}
 		</div>
 	)
 }
 
-export default connect(({ settings }: ConnectState) => ({
-	theme: settings.navTheme,
-	layout: settings.layout,
-}))(GlobalHeaderRight)
+export default GlobalHeaderRight
