0952ed47fd4f6d23d84a00cd7058951091ed5df7
ozkanonur <onurozkan.dev@outlook.com>
2020-04-11 19:58:46 +0300

    [client] Route to the spesific feed by string query

--- client/src/pages/Feeds/Feed/index.tsx
+++ client/src/pages/Feeds/Feed/index.tsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import FeedHeader from './components/FeedHeader'
 import FeedEntries from './components/FeedEntries'
+import { fetchEntriesByTitleSlug, fetchTitleBySlug, fetchOneCategoryById } from '@/services/api'
+import { PageLoading } from '@ant-design/pro-layout'
 
-const Feed: React.FC = () => {
+const Feed: React.FC = ({ computedMatch }) => {
+	const [title, setTitle]: any = useState(null)
+	const [category, setCategory]: any = useState(null)
+	const [entryList, setEntryList]: any = useState(null)
+	useEffect(() => {
+		fetchTitleBySlug(computedMatch.params.feedSlug).then(res => {
+			fetchOneCategoryById(res.data.attributes.category_id).then(res => setCategory(res.data))
+			setTitle(res.data)
+		})
+		fetchEntriesByTitleSlug(computedMatch.params.feedSlug).then(res => setEntryList(res.data.attributes))
+	}, [])
+
+	if (!entryList || !title || !category) return <PageLoading />
+
 	return (
 		<>
-			<FeedHeader />
-			<FeedEntries />
+			<FeedHeader categoryData={category} titleData={title} />
+			<FeedEntries entryData={entryList} titleData={title} />
 		</>
 	)
 }
