bec02fbeeb620f15679a42420ff18b2ddc162f59
ozkanonur <onurozkan.dev@outlook.com>
2020-04-16 18:37:28 +0300

    [client] Add categoryList to global state

--- client/src/redux/Actions/Global/types.ts
+++ client/src/redux/Actions/Global/types.ts
@@ -1,8 +1,14 @@
 export const SET_ACCESS_TOKEN = 'SET_ACCESS_TOKEN'
+export const SET_CATEGORY_LIST = 'SET_CATEGORY_LIST'
 
 export interface SetAccessTokenAction {
 	type: typeof SET_ACCESS_TOKEN
 	token: string
 }
 
-export type GlobalActions = SetAccessTokenAction
+export interface SetCategoryListAction {
+	type: typeof SET_CATEGORY_LIST
+	list: []
+}
+
+export type GlobalActions = SetAccessTokenAction | SetCategoryListAction
