eaaa4b947dcb33d388cf9da153917e79d7536d6c
ozkanonur <onurozkan.dev@outlook.com>
2020-04-22 14:54:17 +0300

    [server] Create AwsService class as shared

---
+++ server/src/shared/Services/aws.service.ts
@@ -0,0 +1,29 @@
+// Other dependencies
+import * as s3 from 'aws-sdk/clients/s3'
+
+// Local files
+import { configService } from './config.service'
+
+export class AwsService {
+    private s3Instance() {
+        return new s3({
+            accessKeyId: configService.getEnv('AWS_S3_ACCESS_KEY'),
+            secretAccessKey: configService.getEnv('AWS_S3_SECRET_KEY'),
+            region: configService.getEnv('AWS_S3_REGION')
+        })
+    }
+
+    getPictureBuffer(fileName: string): unknown {
+        return new Promise((resolve) => {
+            this.s3Instance().getObject({ Bucket: configService.getEnv('AWS_S3_BUCKET'), Key: `${fileName}.jpg`}, (error, data) => {
+                if (error) {
+                    this.s3Instance().getObject({ Bucket: configService.getEnv('AWS_S3_BUCKET'), Key: 'default.jpg' }, (_e, data) => {
+                        resolve(data.Body)
+                    })
+                    return
+                }
+                resolve(data.Body)
+            })
+        })
+    }
+}
