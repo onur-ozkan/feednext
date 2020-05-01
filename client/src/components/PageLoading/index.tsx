e9a4f6345a1ca7176a3b09cd1c5af4d0bfa84063
Nimda <onurozkan.dev@outlook.com>
2020-05-01 21:13:10 +0300

    [client] Update loading pages

--- client/src/components/PageLoading/index.tsx
+++ client/src/components/PageLoading/index.tsx
@@ -1,5 +1,5 @@
-import { PageLoading } from '@ant-design/pro-layout'
+import React from 'react'
 
-// loading components from code split
-// https://umijs.org/plugin/umi-plugin-react.html#dynamicimport
+const PageLoading: React.FC = () => <></>
+
 export default PageLoading
