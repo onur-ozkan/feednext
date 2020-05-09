6f485b8aa656da2cf5d96268268df605cf0a5276
ozkanonur <onurozkan.dev@outlook.com>
2020-05-09 14:45:23 +0300

    [server] Create createMessage function in MessagesRepository

--- server/src/shared/Repositories/messages.repository.ts
+++ server/src/shared/Repositories/messages.repository.ts
@@ -6,5 +6,13 @@ import { MessagesEntity } from '../Entities/messages.entity'
 
 @EntityRepository(MessagesEntity)
 export class MessagesRepository extends Repository<MessagesEntity> {
+    async createMessage({ conversationId, text }: { conversationId: string, text: string }) {
+        const newMessage: MessagesEntity = new MessagesEntity({
+            conversation_id: conversationId,
+            text
+        })
+
+        await this.save(newMessage)
+    }
 
 }
