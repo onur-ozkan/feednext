64cd36327de839770dbba665abfae8bdff0f2f07
ozkanonur <onurozkan.dev@outlook.com>
2020-05-09 14:45:03 +0300

    [server] Create getConversationByParticipants and createConversation functions in ConversationsRepository

--- server/src/shared/Repositories/conversations.repository.ts
+++ server/src/shared/Repositories/conversations.repository.ts
@@ -7,4 +7,25 @@ import { ConversationsEntity } from '../Entities/conversations.entity'
 @EntityRepository(ConversationsEntity)
 export class ConversationsRepository extends Repository<ConversationsEntity> {
 
+    async getConversationByParticipants(participantX: string, participantY: string): Promise<ConversationsEntity> {
+        const conversation = await this.findOneOrFail({
+            where: {
+                $or: [
+                    { participants: [participantX, participantY] },
+                    { participants: [participantY, participantX] }
+                ]
+            }
+        })
+
+        return conversation
+    }
+
+    async createConversation(participants: string[]): Promise<ConversationsEntity> {
+        const newConversation: ConversationsEntity = new ConversationsEntity({
+            participants
+        })
+
+        await this.save(newConversation)
+        return newConversation
+    }
 }
