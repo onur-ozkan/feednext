3feb1b5eed212561907e29f58ea53101af1b04a5
ozkanonur <onurozkan.dev@outlook.com>
2020-05-09 14:48:17 +0300

    [server] Handle message processing in MessageService

--- server/src/v1/Message/Service/message.service.ts
+++ server/src/v1/Message/Service/message.service.ts
@@ -1,7 +1,31 @@
 // Nest dependencies
 import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { UsersRepository } from 'src/shared/Repositories/users.repository'
+import { ConversationsRepository } from 'src/shared/Repositories/conversations.repository'
+import { MessagesRepository } from 'src/shared/Repositories/messages.repository'
 
 @Injectable()
 export class MessageService {
+    constructor(
+        @InjectRepository(UsersRepository)
+        private readonly usersRepository: UsersRepository,
+        @InjectRepository(ConversationsRepository)
+        private readonly conversationsRepository: ConversationsRepository,
+        @InjectRepository(MessagesRepository)
+        private readonly messagesRepository: MessagesRepository,
+    ) {}
 
+    async sendMessage({ recipient, body, from } : { recipient: string, body: string, from: string }) {
+        await this.usersRepository.findOneOrFail({ username: recipient })
+        const conversation = await this.conversationsRepository.getConversationByParticipants(from, recipient)
+            // tslint:disable-next-line:no-empty
+            .catch(_error => {})
+        if (conversation) {
+            await this.messagesRepository.createMessage({ conversationId: String(conversation._id), text: body })
+        } else {
+            const newConversation = await this.conversationsRepository.createConversation([from, recipient])
+            await this.messagesRepository.createMessage({ conversationId: String(newConversation._id), text: body })
+        }
+    }
 }
