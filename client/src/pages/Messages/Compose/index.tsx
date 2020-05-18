b33adc5264936b357db583cf9f282cee9790460d
ozkanonur <onurozkan.dev@outlook.com>
2020-05-18 14:54:44 +0300

    [client] Provide types for Compose page

--- client/src/pages/Messages/Compose/index.tsx
+++ client/src/pages/Messages/Compose/index.tsx
@@ -4,51 +4,34 @@ import { ArrowLeftOutlined } from '@ant-design/icons'
 
 // Other dependencies
 import React, { useEffect, useState } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
 import { history } from 'umi'
 
 // Local files
-import { searchUser, fetchUserByUsername, fetchUnreadMessageInfo } from '@/services/api'
-import { API_URL } from '@/../config/constants'
+import { searchUser, fetchUserByUsername } from '@/services/api'
+import { FormDataType, ComponentProps } from './types'
 import { socketConnection } from '@/services/socket'
-import { SET_UNREAD_MESSAGES_INFO } from '@/redux/Actions/Global'
 import { PageHelmet } from '@/components/PageHelmet'
+import { API_URL } from '@/../config/constants'
 
-export declare interface FormDataType {
-	recipient: string
-	body: string
-}
 
-const Compose = (): JSX.Element => {
-	const [userFilterInput, setUserFilterInput] = useState<string>('')
+const Compose: React.FC<ComponentProps> = (props): JSX.Element => {
+	const globalState = useSelector((state: any) => state.global)
+	const wss = socketConnection(globalState.accessToken)
+	const [form] = Form.useForm()
+
+	const [userFilterInput, setUserFilterInput] = useState<string>(props.history.location.state?.defaultUsername || '')
 	const [autoCompleteData, setAutoCompleteData] = useState(undefined)
-	const [messageForm, setMessageForm] = useState({
+	const [messageForm, setMessageForm] = useState<{ to: string | null, body: string | null }>({
 		to: null,
 		body: null,
 	})
-	const globalState = useSelector((state: any) => state.global)
-
-	const wss = socketConnection(globalState.accessToken)
-	const [form] = Form.useForm()
-	const dispatch = useDispatch()
-
 
 	const handleMessageSending = async (formValues: FormDataType): Promise<void> => {
 		await form.validateFields()
 		await fetchUserByUsername(formValues.recipient).then(() => {
 			wss.emit('sendMessage', formValues)
-			// Refresh unread message state with new conversation before routing there
-			setTimeout(() => {
-				fetchUnreadMessageInfo(globalState.accessToken).then(({ data }) => {
-					dispatch({
-						type: SET_UNREAD_MESSAGES_INFO,
-						data: data.attributes
-					})
-					history.push({
-						pathname: '/messages',
-					})
-				})
-			}, 300)
+			location.href = '/messages'
 		})
 		.catch(_error => message.error('User not found'))
 	}
@@ -106,7 +89,7 @@ const Compose = (): JSX.Element => {
 					style={{ marginBottom: 25 }}
 				/>
 
-				<Form form={form} onFinish={handleMessageSending}>
+				<Form form={form} onFinish={handleMessageSending} initialValues={{ recipient: userFilterInput }}>
 					<Form.Item
 						style={{ marginBottom: 10 }}
 						name="recipient"
