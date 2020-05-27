84c4e54e477df33f03d2dfa8d2922340bce76b33
ozkanonur <onurozkan.dev@outlook.com>
2020-05-27 21:29:49 +0300

    [client] Delete Support & About links untill version 1

--- client/src/pages/Feeds/components/AdditionalBlock/index.tsx
+++ client/src/pages/Feeds/components/AdditionalBlock/index.tsx
@@ -3,32 +3,19 @@ import { Card, Row, Typography, Col } from 'antd'
 
 // Other dependencies
 import React from 'react'
-import { Link } from 'umi'
 
 export const AdditionalBlock = (): JSX.Element => (
 	<Card>
 		<Row style={{ marginBottom: 15 }}>
 			<Col span={12}>
-				<Link to="/about">
+				<a href="https://github.com/feednext/feednext" target="_api">
 					<Typography.Text strong>
-						About
-					</Typography.Text>
-				</Link>
-			</Col>
-			<Col span={12}>
-				<a href="https://github.com/feednext/feednext#readme" target="_api">
-					<Typography.Text strong>
-						API
+						Project Source
 					</Typography.Text>
 				</a>
 			</Col>
 			<Col span={12}>
-				<Typography.Text strong>
-					Support
-				</Typography.Text>
-			</Col>
-			<Col span={12}>
-				<a href="https://github.com/feednext/feednext/blob/master/COPYING" target="_license">
+				<a href="https://raw.githubusercontent.com/feednext/feednext/master/COPYING" target="_license">
 					<Typography.Text strong>
 						License
 					</Typography.Text>
