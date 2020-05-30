75a0592a6f97d1bd79830112f1c80c27b519b600
ozkanonur <onurozkan.dev@outlook.com>
2020-05-30 23:40:43 +0300

    Enable google analytics

--- client/src/layouts/BaseLayout.tsx
+++ client/src/layouts/BaseLayout.tsx
@@ -1,17 +1,23 @@
 // Other dependencies
 import React from 'react'
+import ReactGA from 'react-ga'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 
 // Local files
 import { store, persistor } from '@/redux/store'
 
-const BaseLayout: React.FC = ({ children }) => (
-	<Provider store={store}>
-		<PersistGate loading={null} persistor={persistor}>
-			{children}
-		</PersistGate>
-	</Provider>
-)
+const BaseLayout: React.FC = ({ children }) => {
+	ReactGA.initialize('UA-168030814-1')
+	ReactGA.pageview(window.location.pathname + window.location.search)
+
+	return (
+		<Provider store={store}>
+			<PersistGate loading={null} persistor={persistor}>
+				{children}
+			</PersistGate>
+		</Provider>
+	)
+}
 
 export default BaseLayout
