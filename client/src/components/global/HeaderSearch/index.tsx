479798f0800a9df7b47cab897bf261b84b8000e1
ozkanonur <onurozkan.dev@outlook.com>
2020-06-11 00:53:08 +0300

    [client] Add keyboard controlling to searchbox

--- client/src/components/global/HeaderSearch/index.tsx
+++ client/src/components/global/HeaderSearch/index.tsx
@@ -5,6 +5,7 @@ import { SearchOutlined } from '@ant-design/icons'
 // Other dependencies
 import React, { useRef, useState, useEffect } from 'react'
 import classNames from 'classnames'
+import { useRouter } from 'next/router'
 
 // Local files
 import { searchTitle } from '@/services/api'
@@ -12,10 +13,12 @@ import './style.less'
 
 const HeaderSearch: React.FC = (): JSX.Element => {
 	const inputEl = useRef(null)
+	const router = useRouter()
 
 	const [value, setValue] = useState('')
 	const [searchMode, setSearchMode] = useState(false)
 	const [autoCompleteData, setAutoCompleteData] = useState(null)
+	const [noDataMessage, setNoDataMessage] = useState('')
 
 	const enterSearchMode = (): void => {
 		if (autoCompleteData) return
@@ -30,23 +33,26 @@ const HeaderSearch: React.FC = (): JSX.Element => {
 
 	useEffect(() => {
 		if (value === '') setAutoCompleteData(null)
-
-		if (value.length >= 3) {
+		if (value.length < 3) setNoDataMessage('Enter at least 3 characters to search')
+		else {
 			searchTitle(value).then(res => {
 				const foundTitles = res.data.attributes.titles.map(title => {
 					return {
-						label:
-							<a style={{ width: '100%' }} href={`/${title.slug}`}>
-								<Typography.Text style={{ fontSize: 15 }}> {title.name} </Typography.Text>
-							</a>,
-						options: []
+						label: <Typography.Text style={{ fontSize: 15.7, width: '100%' }}> {title.name} </Typography.Text>,
+						value: title.slug
 					}
 				})
 				if (foundTitles.length === 0) setAutoCompleteData(null)
 				else setAutoCompleteData(foundTitles)
-			})
+				setNoDataMessage('Could not match anything')
+			}).catch(_error => {})
 		}
 	}, [value])
+
+	const handleOnSelect = (titleSlug: string): void => {
+		router.replace(`/${titleSlug}`)
+		router.reload()
+	}
 
 	const inputClass = classNames('input', {
 		'show': searchMode,
@@ -58,14 +64,20 @@ const HeaderSearch: React.FC = (): JSX.Element => {
 	return (
 		<div className={inputClass} onClick={enterSearchMode}>
 			<AutoComplete
+				notFoundContent={
+					<Typography.Text strong style={{ width: '100%' }}>{noDataMessage}</Typography.Text>
+				}
+				backfill
 				className={inputClass}
 				value={value}
 				onChange={(value: string): void => setValue(value)}
+				onSelect={handleOnSelect}
 				onBlur={leaveSearchMode}
 				options={autoCompleteData}
 			>
 				<Input
 					ref={inputEl} placeholder="Search Feeds.."
+					onPressEnter={(): void => handleOnSelect(autoCompleteData[0].value)}
 					onBlur={leaveSearchMode}
 				/>
 			</AutoComplete>
