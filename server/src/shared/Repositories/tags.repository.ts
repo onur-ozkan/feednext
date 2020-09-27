7670513d4056e569c6dc347679f20a0bd0e137d9
ozkanonur <onurozkan.dev@outlook.com>
2020-09-27 11:41:00 +0300

    Implement popularity calculation logic of tags

--- server/src/shared/Repositories/tags.repository.ts
+++ server/src/shared/Repositories/tags.repository.ts
@@ -8,7 +8,7 @@ import { TagsEntity } from '../Entities/tags.entity'
 export class TagsRepository extends Repository<TagsEntity> {
 
     async tagActionOnTitleCreate(tagName: string): Promise<void> {
-        const tag = await this.findOne({ name: tagName })
+        const tag: any = await this.findOne({ name: tagName })
 
         if (!tag) {
             this.save({
@@ -16,8 +16,15 @@ export class TagsRepository extends Repository<TagsEntity> {
                 popularity_ratio: 0.01,
                 total_title: 0
             })
-        } else {
-            // TODO popularity_ratio logic
+        }
+
+        else {
+            const diff = new Date().getTime() - tag.updated_at
+            const diffAsMin = Math.round((diff / 1000) / 60)
+
+            if (diffAsMin <= 1440 && tag.popularity_ratio < 1.00) tag.popularity_ratio += 0.01
+            else tag.popularity_ratio = 0.02
+
             tag.total_title++
             this.save(tag)
         }
