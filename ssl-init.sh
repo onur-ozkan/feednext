aae4e1e778c97fd370d3c5f8e4f4d6ebb98ea1b5
Onur <onurozkan.dev@outlook.com>
2020-10-23 20:12:49 +0300

    Update ssl-init.sh

--- ssl-init.sh
+++ ssl-init.sh
@@ -30,7 +30,7 @@ fi
 echo "### Creating dummy certificate for $domains ..."
 path="/etc/letsencrypt/live/$domains"
 mkdir -p "$data_path/conf/live/$domains"
-docker-compose -f docker-compose.dev.yml run --rm --entrypoint "\
+docker-compose -f docker-compose.prod.yml run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:1024 -days 1\
     -keyout '$path/privkey.pem' \
     -out '$path/fullchain.pem' \
@@ -43,7 +43,7 @@ docker-compose -f docker-compose.dev.yml up --force-recreate -d nginx_dev
 echo
 
 echo "### Deleting dummy certificate for $domains ..."
-docker-compose -f docker-compose.dev.yml run --rm --entrypoint "\
+docker-compose -f docker-compose.prod.yml run --rm --entrypoint "\
   rm -Rf /etc/letsencrypt/live/$domains && \
   rm -Rf /etc/letsencrypt/archive/$domains && \
   rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
@@ -66,7 +66,7 @@ esac
 # Enable staging mode if needed
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
-docker-compose -f docker-compose.dev.yml run --rm --entrypoint "\
+docker-compose -f docker-compose.prod.yml run --rm --entrypoint "\
   certbot certonly --webroot -w /var/www/certbot \
     $staging_arg \
     $email_arg \
@@ -76,5 +76,4 @@ docker-compose -f docker-compose.dev.yml run --rm --entrypoint "\
     --force-renewal" certbot
 echo
 
-echo "### Reloading nginx ..."
-docker-compose -f docker-compose.dev.yml exec nginx nginx -s reload
+echo "### Restart the containers."
